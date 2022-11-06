49051b42720153788d25d4acb92be60b102be958
C1l1x <pavlov_s@abv.bg>
2022-11-06 17:58:06 +0200

    init

--- Osn_soft_inj/UP_2_3/zadacha6.cpp
+++ Osn_soft_inj/UP_2_3/zadacha6.cpp
@@ -1,30 +1,51 @@
-#include <iostream>
+#include <iostream> 
 
 using namespace std;
 
 int main(){
-    int ele_count; 
-    cout << "Insert elements count: ";
-    cin >> ele_count; // Взимаме брия на елементите в масива
-    int array[ele_count], i, j;
-    for (i = 1; i <= ele_count; i++){
-        cout << "Element " << i  << " :";
-        cin >> array[i]; // задаваме стойност на елементите в масива
+    int array1[21],array2[21]; 
+    int ele_count;
+    cout << "Insert element count: ";
+    cin >> ele_count;
+    while (ele_count>21)
+    {
+        cout << "Elements should'nt be more than 21!\n";
+        cout << "Insert element count: ";
+        cin >> ele_count;
+    }
+    
+    int i, j;
+    for (i = 1; i <= ele_count; i++)
+    {
+        cout << "Element " << i << " :";
+        cin >> array1[i];
+        array2[i] = 0;
+        int save_ele = array1[i];
+        while (save_ele > 0)
+        {
+            array2[i] += (save_ele % 10);
+            save_ele /= 10;
+        }
     }
     int check;
-    do{
+    do
+    {
         check = 0;
         for (i = 1; i < ele_count; i++)
-        // търсим по-малкото число
-            if (array[i] > array[i + 1]){
-                int new_array;
-                new_array = array[i]; // запазваме числото 
-                array[i] = array[i + 1]; // местим по-малкото число в началото на масива
-                array[i + 1] = new_array; 
+            if ((array2[i] > array2[i + 1]) || ((array2[i] == array2[i + 1]) && (array1[i] > array1[i + 1])))
+            {
+                int save_ele;
+                save_ele = array1[i];
+                array1[i] = array1[i + 1];
+                array1[i + 1] = save_ele;
+                save_ele = array2[i];
+                array2[i] = array2[i + 1];
+                array2[i + 1] = save_ele;
                 check = 1;
             }
     } while (check);
+    cout << "Result: ";
     for (i = 1; i <= ele_count; i++)
-        cout << array[i] << " "; // изкарваме числата от низходящ ред
+        cout << array1[i] << " ";
     cout << "\n";
 }
